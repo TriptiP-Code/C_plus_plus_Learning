783b62c0e82b63aeafffd4dd229391a5e2502c65
TriptiP-Code <triptip033@gmail.com>
2026-08-29 15:38:48 +0530

    cin and getline

--- second.cpp
+++ second.cpp
@@ -2,6 +2,31 @@
 using namespace std;
 
 int main(){
-  cout<<"hello world";
+string s;
+cout<<"enter name"<<endl;
+cin>>s;
+cout<<"the name is "<<s;
   return 0;
 }
+
+// the output is 
+// enter name
+// tripti pandey
+// the name is tripti
+// the space tells that input is over
+
+// instead use getline() to take complete statement input
+
+// example
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     string statement;
+//     getline(cin, statement);
+
+//     cout << statement;
+
+//     return 0;
+// }
