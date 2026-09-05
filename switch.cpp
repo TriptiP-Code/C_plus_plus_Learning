bbe6712770b5c0ffb6b216e91701d1579cac8760
TriptiP-Code <triptip033@gmail.com>
2026-09-05 16:00:01 +0530

    conditional statements

---
+++ switch.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  char c ='B';
+  switch (c){
+
+    case 'A':
+    cout<<"GFG";
+    break;
+
+    case 'B' :
+    cout << "greesksforgreeeks"
+    break;
+  }
+  return 0;
+}
