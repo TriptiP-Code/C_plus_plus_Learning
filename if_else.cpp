e4b643c3e539d63c434e2fc6432d6fe9cca06b49
TriptiP-Code <triptip033@gmail.com>
2026-09-06 12:29:10 +0530

    if else statement

---
+++ if_else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int n =5;
+
+  if(n>0){
+    cout<<"number is possitive";
+  }
+  else {
+    cout <<"number is non positive";
+  }
+
+  return 0;
+}
