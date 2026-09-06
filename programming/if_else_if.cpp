71f79a9cbd264df236be2d356e948d9b5cbcaad5
TriptiP-Code <triptip033@gmail.com>
2026-09-06 13:11:11 +0530

    moving files

---
+++ programming/if_else_if.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int age =18;
+  if (age <13)
+  {
+    cout <<"child";
+  }
+  else if (age >=1 and age <= 18){
+    cout<<"growing stage";
+  }
+  else{
+    cout<<"adult";
+  }
+  return 0;
+}
