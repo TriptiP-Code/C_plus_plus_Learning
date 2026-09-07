f3c08bd75cd3db8d1e0fb0d360d4a1202ab515aa
TriptiP-Code <triptip033@gmail.com>
2026-09-07 15:44:33 +0530

    infinite loop

---
+++ programming/infinite_loop.cpp
@@ -0,0 +1,12 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   for(;;)
+//   {
+//     cout<<"this loop will run forever.\n";
+//   }
+//   return 0;
+// }
+infinite loop
