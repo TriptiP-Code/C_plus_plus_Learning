fa6cb95e70c30ed45eae329171c334977ae597ff
TriptiP-Code <triptip033@gmail.com>
2026-09-08 14:07:35 +0530

    funcation calling programming

---
+++ programming/calling_function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void greet()
+{
+  cout<<"welcome to c++ programming"<<endl;
+}
+int multiply(int a , int b)
+{
+  return a*b;
+}
+
+int main()
+{
+  greet();
+  int result =multiply (4,5);
+  cout<<"mu;tiplication result"<<result <<endl;
+  return 0;
+}
