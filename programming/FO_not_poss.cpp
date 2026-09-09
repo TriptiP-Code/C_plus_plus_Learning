215166e89613a2d58cb05a15061dc3ef18fdd764
TriptiP-Code <triptip033@gmail.com>
2026-09-09 15:08:36 +0530

    function overloading cannot overloaded programming

---
+++ programming/FO_not_poss.cpp
@@ -0,0 +1,20 @@
+// #include <iostream>
+
+// using namespace std;
+
+// string get ()
+// {
+//   return "this returns string";
+// }
+// int get()
+// {
+//   return 99;
+// }
+
+// int main()
+// {
+//   string s =get()
+//   cout <<s;
+
+//   return 0;
+// }
