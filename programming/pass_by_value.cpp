378fd5104e8e243c3eabc7f711f31eb62f6a3823
TriptiP-Code <triptip033@gmail.com>
2026-09-10 14:44:00 +0530

    commit5

--- programming/pass_by_value.cpp
+++ programming/pass_by_value.cpp
@@ -13,3 +13,7 @@ int main()
   cout<<x;
   return 0;
 }
+
+// in this program , when the change function is called with x as the argument a copy of x is created and passed to the function 
+// Inside the function , the parameter a is modified but this modification only affects the local copy of the valye , not the original variable x , as demonstrated by the output 
+
