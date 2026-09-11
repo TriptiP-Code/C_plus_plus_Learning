1a86e4f79d3523e9345560a57c1154ec1244a9ad
TriptiP-Code <triptip033@gmail.com>
2026-09-11 14:53:06 +0530

    c2

--- programming/default_args.cpp
+++ programming/default_args.cpp
@@ -12,3 +12,6 @@ int main()
   f(221);
   return 0;
 }
+
+// in this program , the function f has a default argument a=10 , so when no argument is provided a defaults to 10 which is printed . 
+// when 221 is passed , a becomes 221 which is then printed  
