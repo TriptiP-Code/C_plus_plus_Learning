3268f93f3c851e74e5367ac997fa76f24788c768
TriptiP-Code <triptip033@gmail.com>
2026-09-13 18:22:55 +0530

    c7

--- programming/Recursion.cpp
+++ programming/Recursion.cpp
@@ -12,3 +12,7 @@ int main()
   printHello(5);
   return 0;
 }
+
+// this demonstrates a simple recursive function that prints hello multiple times 
+// the function calls itself with a smaller vvalue of n until the base case n ==0 is reached 
+// each recursive call prints "hello" ince resulting in the message neing printed 5 times
