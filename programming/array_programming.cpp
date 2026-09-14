1437aaa4bff4a05d0bf7ea7eaee4aeb8d4462731
TriptiP-Code <triptip033@gmail.com>
2026-09-14 13:05:11 +0530

    c5

--- programming/array_programming.cpp
+++ programming/array_programming.cpp
@@ -8,3 +8,14 @@ int main(){
   }
   return 0;
 }
+
+// explanation :
+
+// int arr[5] declares an array of 5 integers 
+
+// the elemets are initialized with {2,4,8,12,16}
+
+// the loop is used to iterateover the array and print each element 
+
+// array indicies in c++ start from 0 , so arr[0] refers to the 1st lement , and arr[4] refers to the last one in this case
+
