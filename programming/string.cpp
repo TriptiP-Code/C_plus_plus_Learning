7ab407e33738f58a4ef4f3987e830a8e8856b0bc
TriptiP-Code <triptip033@gmail.com>
2026-09-19 15:33:53 +0530

    string program

---
+++ programming/string.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string str="hello everyone";
+  cout<<"using index: ";
+  for (int i=0;i<str.size();i++)
+  {
+    cout<<str[i];
+  }
+  cout<<"using range based for loop: ";
+  for(char ch : str)
+  {
+    cout<<ch;
+  }
+  cout<<endl;
+  cout<<"using iterator: ";
+  for(auto it =str.begin(); it != str.endl() ; it++){
+    cout<<*it;
+  }
+  cout<<endl;
+  return 0;
+}
