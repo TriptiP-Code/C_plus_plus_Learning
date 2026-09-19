d0d63ddd3a060b8cea5c9140f5b5520a585beee7
TriptiP-Code <triptip033@gmail.com>
2026-09-19 16:27:47 +0530

    string length programing

---
+++ programming/str_length.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  string str ="hello guys";
+  cout <<"length using size() : "<< str.size()<<endl;
+
+  cout<<"length using length(): "<<str.length()<<endl;
+
+  return 0;
+}
