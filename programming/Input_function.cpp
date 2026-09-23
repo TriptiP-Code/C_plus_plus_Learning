c9ed7a19e1dc95571c4def2f3117f02177f9262f
TriptiP-Code <triptip033@gmail.com>
2026-09-23 17:16:35 +0530

    input functions  strings programming 8

--- programming/Input_function.cpp
+++ programming/Input_function.cpp
@@ -22,6 +22,12 @@ int main()
   cout<<"the string after pop_back operation is : ";
 
   cout<<str<<endl;
-  
+
   return 0;
 }
+
+// here getline()  reads the complete line push_back() adds a character to the end and pop_back()  removes the last character 
+
+// time complexity: O(1)
+// Space complexity: o(n) where n is the size of string
+
